c6c2dfa6fe6634f41c910be286224aa6d8a2543d
Huỳnh Công Quyền <congquyenhuynh111@gmail.com>
2022-12-09 17:24:24 +0700

    ver2

--- ML_Healthcare.py
+++ ML_Healthcare.py
@@ -28,36 +28,36 @@ start_time=time.time()  #Program Start time
 tit1,tit2 = st.columns((4, 1))
 tit1.markdown("<h1 style='text-align: center;'><u>Machine Learning in Healthcare</u> </h1>",unsafe_allow_html=True)
 tit2.image("healthcare2.png")
-st.sidebar.title("Dataset and Classifier")
+st.sidebar.title("Bộ dữ liệu và bộ phân loại")
 
-dataset_name=st.sidebar.selectbox("Select Dataset: ",['Heart Attack',"Breast Cancer"])
-classifier_name = st.sidebar.selectbox("Select Classifier: ",("Logistic Regression","KNN","SVM","Decision Trees",
-                                                              "Random Forest","Gradient Boosting","XGBoost"))
+dataset_name=st.sidebar.selectbox("Chọn tập dữ liệu: ",['Đau tim',"Ung thư vú"])
+classifier_name = st.sidebar.selectbox("Chọn Trình phân loại: ",("Logistic Regression","KNN","SVM","Decision Trees",
+                                                              "Random Forest","Gradient Boosting"))
 
 LE=LabelEncoder()
 def get_dataset(dataset_name):
-    if dataset_name=="Heart Attack":
+    if dataset_name=="Đau tim":
         data=pd.read_csv("Data/heart.csv")
-        st.header("Heart Attack Prediction")
+        st.header("Dự đoán cơn đau tim")
         return data
 
-    elif dataset_name=="Breast Cancer":
+    else:
         data=pd.read_csv("Data/BreastCancer.csv")
         data["diagnosis"] = LE.fit_transform(data["diagnosis"])
         data.replace([np.inf, -np.inf], np.nan, inplace=True)
         data["diagnosis"] = pd.to_numeric(data["diagnosis"], errors="coerce")
-        st.header("Breast Cancer Prediction")
+        st.header("Dự đoán ung thư vú")
         return data
 
 data = get_dataset(dataset_name)
 
 def selected_dataset(dataset_name):
-    if dataset_name == "Heart Attack":
+    if dataset_name == "Đau tim":
         X=data.drop(["output"],axis=1)
         Y=data.output
         return X,Y
 
-    elif dataset_name == "Breast Cancer":
+    elif dataset_name == "Ung thư vú":
         X = data.drop(["id","diagnosis"], axis=1)
         Y = data.diagnosis
         return X,Y
@@ -68,26 +68,26 @@ X,Y=selected_dataset(dataset_name)
 def plot_op(dataset_name):
     col1, col2 = st.columns((1, 5))
     plt.figure(figsize=(12, 3))
-    plt.title("Classes in 'Y'")
-    if dataset_name == "Heart Attack":
+    plt.title("Các lớp trong tập dữ liệu")
+    if dataset_name == "Đau tim":
         col1.write(Y)
         sns.countplot(Y, palette='gist_heat')
         col2.pyplot()
 
-    elif dataset_name == "Breast Cancer":
+    elif dataset_name == "Ung thư vú":
         col1.write(Y)
         sns.countplot(Y, palette='gist_heat')
         col2.pyplot()
 
 st.write(data)
-st.write("Shape of dataset: ",data.shape)
-st.write("Number of classes: ",Y.nunique())
+st.write("Hình dạng của tập dữ liệu: ",data.shape)
+st.write("Số lớp: ",Y.nunique())
 plot_op(dataset_name)
 
 
 def add_parameter_ui(clf_name):
     params={}
-    st.sidebar.write("Select values: ")
+    st.sidebar.write("Chọn giá trị: ")
 
     if clf_name == "Logistic Regression":
         R = st.sidebar.slider("Regularization",0.1,10.0,step=0.1)
@@ -131,24 +131,6 @@ def add_parameter_ui(clf_name):
         params["L"] = L
         params["M"] = M
 
-    elif clf_name == "XGBoost":
-        N = st.sidebar.slider("n_estimators", 50, 500, step=50, value=50)
-        LR = st.sidebar.slider("Learning Rate", 0.01, 0.5,value=0.1)
-        O = st.sidebar.selectbox("Objective", ('binary:logistic','reg:logistic','reg:squarederror',"reg:gamma"))
-        M = st.sidebar.slider("max_depth", 1, 20,value=6)
-        G = st.sidebar.slider("Gamma",0,10,value=5)
-        L = st.sidebar.slider("reg_lambda",1.0,5.0,step=0.1)
-        A = st.sidebar.slider("reg_alpha",0.0,5.0,step=0.1)
-        CS = st.sidebar.slider("colsample_bytree",0.5,1.0,step=0.1)
-        params["N"] = N
-        params["LR"] = LR
-        params["O"] = O
-        params["M"] = M
-        params["G"] = G
-        params["L"] = L
-        params["A"] = A
-        params["CS"] = CS
-
     RS=st.sidebar.slider("Random State",0,100)
     params["RS"] = RS
     return params
@@ -174,10 +156,6 @@ def get_classifier(clf_name,params):
 
     elif clf_name == "Gradient Boosting":
         clf = GradientBoostingClassifier(n_estimators=params["N"],learning_rate=params["LR"],loss=params["L"],max_depth=params["M"])
-
-    elif clf_name == "XGBoost":
-        clf = XGBClassifier(booster="gbtree",n_estimators=params["N"],max_depth=params["M"],learning_rate=params["LR"],
-                            objective=params["O"],gamma=params["G"],reg_alpha=params["A"],reg_lambda=params["L"],colsample_bytree=params["CS"])
 
     return clf
 
@@ -209,29 +187,29 @@ def compute(Y_pred,Y_test):
     x2 = X_projected[:,1]
     plt.figure(figsize=(16,8))
     plt.scatter(x1,x2,c=Y,alpha=0.8,cmap="viridis")
-    plt.xlabel("Principal Component 1")
-    plt.ylabel("Principal Component 2")
+    plt.xlabel("Thành phần chính 1")
+    plt.ylabel("Thành phần chính 2")
     plt.colorbar()
     st.pyplot()
 
     c1, c2 = st.columns((4,3))
     #Output plot
     plt.figure(figsize=(12,6))
-    plt.scatter(range(len(Y_pred)),Y_pred,color="yellow",lw=5,label="Predictions")
-    plt.scatter(range(len(Y_test)),Y_test,color="red",label="Actual")
-    plt.title("Prediction Values vs Real Values")
+    plt.scatter(range(len(Y_pred)),Y_pred,color="yellow",lw=5,label="Phỏng đoán")
+    plt.scatter(range(len(Y_test)),Y_test,color="red",label="Thật sự")
+    plt.title("Giá trị dự đoán so với giá trị thực")
     plt.legend()
     plt.grid(True)
     c1.pyplot()
 
     #Confusion Matrix
     cm=confusion_matrix(Y_test,Y_pred)
-    class_label = ["High-risk", "Low-risk"]
+    class_label = ["rủi ro cao", "rủi ro thấp"]
     df_cm = pd.DataFrame(cm, index=class_label,columns=class_label)
     plt.figure(figsize=(12, 7.5))
     sns.heatmap(df_cm,annot=True,cmap='Pastel1',linewidths=2,fmt='d')
-    plt.title("Confusion Matrix",fontsize=15)
-    plt.xlabel("Predicted")
+    plt.title("Ma trận hỗn loạn",fontsize=15)
+    plt.xlabel("Dự đoán")
     plt.ylabel("True")
     c2.pyplot()
 
@@ -239,30 +217,30 @@ def compute(Y_pred,Y_test):
     acc=accuracy_score(Y_test,Y_pred)
     mse=mean_squared_error(Y_test,Y_pred)
     precision, recall, fscore, train_support = score(Y_test, Y_pred, pos_label=1, average='binary')
-    st.subheader("Metrics of the model: ")
-    st.text('Precision: {} \nRecall: {} \nF1-Score: {} \nAccuracy: {} %\nMean Squared Error: {}'.format(
-        round(precision, 3), round(recall, 3), round(fscore,3), round((acc*100),3), round((mse),3)))
+    st.subheader("Số liệu của mô hình: ")
+    st.text('Độ chính xác: {}  \nSự chính xác: {} %\nSai số toàn phương trung bình: {}'.format(
+        round(precision, 3), round((acc*100),3), round((mse),3)))
 
 st.markdown("<hr>",unsafe_allow_html=True)
-st.header(f"1) Model for Prediction of {dataset_name}")
-st.subheader(f"Classifier Used: {classifier_name}")
+st.header(f"1) Mô hình dự đoán {dataset_name}")
+st.subheader(f"Bộ phân loại được sử dụng: {classifier_name}")
 compute(Y_pred,Y_test)
 
 #Execution Time
 end_time=time.time()
-st.info(f"Total execution time: {round((end_time - start_time),4)} seconds")
+st.info(f"Tổng thời gian thực hiện: {round((end_time - start_time),4)} giây")
 
 #Get user values
 def user_inputs_ui(dataset_name,data):
     user_val = {}
-    if dataset_name == "Breast Cancer":
+    if dataset_name == "Ung thư vú":
         X = data.drop(["id","diagnosis"], axis=1)
         for col in X.columns:
             name=col
             col = st.number_input(col, abs(X[col].min()-round(X[col].std())), abs(X[col].max()+round(X[col].std())))
             user_val[name] = round((col),4)
 
-    elif dataset_name == "Heart Attack":
+    elif dataset_name == "Đau tim":
         X = data.drop(["output"], axis=1)
         for col in X.columns:
             name=col
@@ -273,12 +251,12 @@ def user_inputs_ui(dataset_name,data):
 
 #User values
 st.markdown("<hr>",unsafe_allow_html=True)
-st.header("2) User Values")
-with st.expander("See more"):
+st.header("2) Giá trị người dùng")
+with st.expander("Chi tiết"):
     st.markdown("""
-    In this section you can use your own values to predict the target variable. 
-    Input the required values below and you will get your status based on the values. <br>
-    <p style='color: red;'> 1 - High Risk </p> <p style='color: green;'> 0 - Low Risk </p>
+    Trong phần này, bạn có thể sử dụng các giá trị của riêng mình để dự đoán biến mục tiêu.
+    Nhập các giá trị bắt buộc bên dưới và bạn sẽ nhận được trạng thái của mình dựa trên các giá trị. <br>
+    <p style='color: red;'> 1 - Nguy cơ cao </p> <p style='color: green;'> 0 - Nguy cơ thấp </p>
     """,unsafe_allow_html=True)
 
 user_val=user_inputs_ui(dataset_name,data)
@@ -286,19 +264,19 @@ user_val=user_inputs_ui(dataset_name,data)
 #@st.cache(suppress_st_warning=True)
 def user_predict():
     global U_pred
-    if dataset_name == "Breast Cancer":
+    if dataset_name == "Ung thư vú":
         X = data.drop(["id","diagnosis"], axis=1)
         U_pred = clf.predict([[user_val[col] for col in X.columns]])
 
-    elif dataset_name == "Heart Attack":
+    elif dataset_name == "Đau tim":
         X = data.drop(["output"], axis=1)
         U_pred = clf.predict([[user_val[col] for col in X.columns]])
 
-    st.subheader("Your Status: ")
+    st.subheader("Trạng thái: ")
     if U_pred == 0:
-        st.write(U_pred[0], " - You are not at high risk :)")
+        st.write(U_pred[0], " - Bạn không có nguy cơ cao :)")
     else:
-        st.write(U_pred[0], " - You are at high risk :(")
+        st.write(U_pred[0], " - Bạn có nguy cơ cao :(")
 
 user_predict()  #Predict the status of user.
 
